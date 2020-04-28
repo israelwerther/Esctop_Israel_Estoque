d654fc03bc46f5f3d5aa4dc44b64a9abc96ea3ab
israel <israelwerther48@outlook.com>
2020-04-28 15:36:29 -0300

    Admin do emprestimo configurado parcialmente

--- projeto/emprestimo/admin.py
+++ projeto/emprestimo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Emprestimo
 
-# Register your models here.
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display=(
+        '__str__',
+        'funcionário',
+        'num_doc',
+        'movimento',
+    )
+    search_fields=('num_doc',)
+    list_filter=('funcionário',)
+    date_hierarchy = 'created'
+    
+    
