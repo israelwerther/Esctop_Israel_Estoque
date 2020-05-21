f5ad2a24c4c2b2db2e33570d7b0dec5affac8d60
israel <israelwerther48@outlook.com>
2020-05-21 20:48:13 -0300

    Problema da id vazia resolvida-mascara serão inseridas de novo

--- static/bootstrap/js/masks.js
+++ static/bootstrap/js/masks.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $('.date-mask').mask('00/00/0000');
     $('.date-mask-placeholder').mask('00/00/0000', {placeholder: "__/__/____"});
-    $('.phone-mask').mask('(00) 0000-0000');
-    $('.cel-phone-mask').mask('(00) 00000-0000');
-    $('.cep-mask').mask('00.000-000');
+    $('.phone-mask').mask('(00) 0000-0000');  
     $('.cpf-mask').mask('000.000.000-00');
     $('.datepicker').datepicker({
         dateFormat: "dd/mm/yy",
