8c981fae96faf05e4f5b5997edba75437ac6d265
israel <israelwerther48@outlook.com>
2020-06-07 06:53:36 -0300

    Choices de estado civil inserido

--- projeto/cliente/models.py
+++ projeto/cliente/models.py
@@ -26,9 +26,9 @@ class Cliente(models.Model):
     cpf                   = models.CharField("CPF", max_length=20, unique=True)   
     rg                    = models.CharField("RG",max_length=20, blank=True, null=True)
     data_nasc             = models.DateField("Data de Nascimento",max_length=8, blank=True, null=True)   
-    naturalidade          = models.CharField("naturalidade", max_length=15, choices=ESTADO_CIVIL, blank=True, null=True)
+    naturalidade          = models.CharField("naturalidade", max_length=15, blank=True, null=True)
     
-    estado_civil          = models.CharField("Estado Civil", max_length=15, blank=True, null=True)
+    estado_civil          = models.CharField("Estado Civil", max_length=15, blank=True, null=True, choices=ESTADO_CIVIL)
     
     nome_da_mae           = models.CharField("Nome da Mãe", max_length=50, blank=True, null=True)
     nome_do_pai           = models.CharField("Nome da Pai", max_length=50, blank=True, null=True)
@@ -36,8 +36,7 @@ class Cliente(models.Model):
     rua                   = models.CharField("Rua", max_length=60, blank=True, null=True)
     bairro                = models.CharField("Bairro", max_length=40, blank=True, null=True)
     cidade                = models.CharField("Cidade", max_length=40, blank=True, null=True)
-    uf                    = models.CharField("Estado", max_length=2, blank=True, null=True)
-   
+    uf                    = models.CharField("Estado", max_length=2, blank=True, null=True)   
     numero_casa           = models.CharField("Nº ", max_length=5, blank=True, null=True)
     ponto_referencia      = models.CharField("Ponto de Referencia", max_length=100, blank=True, null=True)
     contato1              = models.CharField("Contato 1",max_length=15, blank=True, null=True)
