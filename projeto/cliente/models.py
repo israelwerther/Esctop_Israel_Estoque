64deff9261c43289044eee604b113115d77b677f
israel <israelwerther48@outlook.com>
2020-06-08 06:44:16 -0300

    Campos que faltavam no avalista e contrato quase terminado

--- projeto/cliente/models.py
+++ projeto/cliente/models.py
@@ -5,7 +5,18 @@ from django.urls import reverse_lazy
 
 class Avalista(models.Model):
     avalista                   = models.CharField("Avalista", max_length=50, blank=True, null=True)
-    cpf_avalista               = models.CharField("CPF do Avalista", max_length=20, unique=True) 
+    cpf_avalista               = models.CharField("CPF do Avalista", max_length=20, unique=True)
+    rg_avalista                    = models.CharField("RG",max_length=20, blank=True, null=True)
+    orgao_expedidor_avalista       = models.CharField("Org Exp", max_length=15, blank=True, null=True)
+    data_nasc_avalista             = models.DateField("Data de Nascimento",max_length=8, blank=True, null=True)   
+    naturalidade_avalista          = models.CharField("naturalidade", max_length=15, blank=True, null=True)    
+    estado_civil_avalista          = models.CharField("Estado Civil", max_length=15, blank=True, null=True) 
+    cep                   = models.CharField("CEP", max_length=10, blank=True, null=True)
+    rua                   = models.CharField("Rua", max_length=60, blank=True, null=True)
+    bairro                = models.CharField("Bairro", max_length=40, blank=True, null=True)
+    cidade                = models.CharField("Cidade", max_length=40, blank=True, null=True)
+    uf                    = models.CharField("Estado", max_length=2, blank=True, null=True)   
+    numero_casa           = models.CharField("Nº ", max_length=5, blank=True, null=True)
         
     class Meta:
         ordering = ('avalista',) 
@@ -22,10 +33,8 @@ class Cliente(models.Model):
     rg                    = models.CharField("RG",max_length=20, blank=True, null=True)
     orgao_expedidor       = models.CharField("Org Exp", max_length=15, blank=True, null=True)
     data_nasc             = models.DateField("Data de Nascimento",max_length=8, blank=True, null=True)   
-    naturalidade          = models.CharField("naturalidade", max_length=15, blank=True, null=True)
-    
-    estado_civil          = models.CharField("Estado Civil", max_length=15, blank=True, null=True)
-    
+    naturalidade          = models.CharField("naturalidade", max_length=15, blank=True, null=True)    
+    estado_civil          = models.CharField("Estado Civil", max_length=15, blank=True, null=True)    
     nome_da_mae           = models.CharField("Nome da Mãe", max_length=50, blank=True, null=True)
     nome_do_pai           = models.CharField("Nome da Pai", max_length=50, blank=True, null=True)
     cep                   = models.CharField("CEP", max_length=10, blank=True, null=True)
