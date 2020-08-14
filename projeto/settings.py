1b1339261dcd4ae251083ef092cb3c5a30e807cd
israel <israelwerther48@outlook.com>
2020-08-14 13:50:49 -0300

    Models e template de representante inseridos

--- projeto/settings.py
+++ projeto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'esctopteste.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'esctopteste.herokuapp.com']
 
 # Application definition
 
--- static/bootstrap/js/masks.js
+++ static/bootstrap/js/masks.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.date-mask').mask('00/00/0000');
     $('.date-mask-placeholder').mask('00/00/0000', {placeholder: "__/__/____"});
     $('.phone-mask').mask('(00) 0000-0000');    
-    $('.cel-phone-mask').mask('(00) 0 0000:0000');    
+    $('.cel-phone-mask').mask('(00) 0 0000-0000');    
     $('.cpf-mask').mask('000.000.000-00');    
     $('.datepicker').datepicker({
         dateFormat: "dd/mm/yy",
