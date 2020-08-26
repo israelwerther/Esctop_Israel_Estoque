2223ba51f4e17d2f3a0f80e2d98ea1f4c38f5381
israel <israelwerther48@outlook.com>
2020-08-26 09:41:53 -0300

    inserindo placeholds em referencias

--- projeto/cliente_cnpj/forms.py
+++ projeto/cliente_cnpj/forms.py
@@ -6,7 +6,9 @@ class Cliente_cnpjForm(forms.ModelForm):
     class Meta:
         model = Cliente_cnpj
         fields = '__all__'
-
-
+        widgets = {
+            'ref1_nome': forms.TextInput(attrs={'placeholder': 'Refencia 1'}),
+            'ref2_nome': forms.TextInput(attrs={'placeholder': 'Refencia 2'}),            
+        }
 
 
