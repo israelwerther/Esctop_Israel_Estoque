120dbe3438bc2af826880d8cf3042b6ae4ed3840
israel <israelwerther48@outlook.com>
2020-09-30 10:37:55 -0300

    Enter desativado na input

--- static/bootstrap/js/masks.js
+++ static/bootstrap/js/masks.js
@@ -10,4 +10,13 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+        }
+    });
+});
+
 
