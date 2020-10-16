2c1fad2890a463eb5275109b182c5dae1cc8b4c1
israel <israelwerther48@outlook.com>
2020-10-16 15:28:49 -0300

    nome fantasia é opcional

--- projeto/cliente_cnpj/models.py
+++ projeto/cliente_cnpj/models.py
@@ -7,7 +7,7 @@ from projeto.core.models import Banco, Tipo_de_conta
 
 class Cliente_cnpj(models.Model):
     razao_social          = models.CharField("Razão Social", max_length=50, blank=True, null=True)
-    nome_fantasia         = models.CharField("Nome Fantasia", max_length=50)
+    nome_fantasia         = models.CharField("Nome Fantasia", max_length=50, blank=True, null=True)
     cnpj                  = models.CharField("CNPJ", max_length=36, unique=True, blank=True, null=True)
     fundacao              = models.DateField("Fundação",max_length=8, blank=True, null=True)      
     inscricao_estadual    = models.CharField("Inscrição Estadual",blank=True, null=True, max_length=50)
